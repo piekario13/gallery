1dfefc6130c42e4bf19f0e0f882c041652915753
Adam Wasilewski <a.wasilewski91@gmail.com>
2018-03-28 21:40:18 +0200

    Dodanie key

--- src/Filter/index.js
+++ src/Filter/index.js
@@ -6,20 +6,22 @@ class Filter extends Component {
   render() {
     console.log(Categories)
     return (
-        <Fragment>
-            <select>
-                <option value="categories">Kategoria</option>
-                <option value="data">Data</option>
-                <option value="alphabet">Alfabetycznie</option>
-            </select>
-            <select>
-                {
-                    Categories.map((item, id)=>{
-                        return <option key={id} value="categories">{item.name}</option>
-                    })
-                }
-            </select>
-        </Fragment>
+      <Fragment>
+        <select>
+          <option value="categories">Kategoria</option>
+          <option value="data">Data</option>
+          <option value="alphabet">Alfabetycznie</option>
+        </select>
+        <select>
+          {Categories.map((item, id) => {
+            return (
+              <option key={id} value={item.id}>
+                {item.name}
+              </option>
+            )
+          })}
+        </select>
+      </Fragment>
     )
   }
 }
