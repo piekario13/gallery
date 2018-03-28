fca30db7a805997f4e8bbb0682069b2fd912c213
piekario13 <piekario13@gmail.com>
2018-03-28 21:37:55 +0200

    stories

--- src/Search/index.js
+++ src/Search/index.js
@@ -1,9 +1,14 @@
 import React, { Component, Fragment } from 'react'
 import './search.css'
+import Categories from '../Stories/Store'
 
 class Search extends Component {
   render() {
-    return <Fragment>Search</Fragment>
+    return (
+    <div className="search">
+      <input name="HeaderInput"/>
+      <button type="button">Szukaj</button>
+    </div> )
   }
 }
 
---
+++ src/Stories/Store.js
@@ -0,0 +1,16 @@
+let Categories=[
+    {
+      id: 0,
+      name: 'samochody'
+    },
+    {
+      id: 1,
+      name: 'budynki'
+    },
+    {
+      id: 2,
+      name: 'ludzie'
+    }
+  ]
+
+  export default Categories
