ac9ab56405e9a311ec418295dca2486f934d821c
Adam Wasilewski <a.wasilewski91@gmail.com>
2018-03-28 20:42:48 +0200

    Poczatki komponentow

---
+++ src/Header/index.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react'
+import Search from '../Search'
+import './header.css'
+
+class Header extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Search />
+      </Fragment>
+    )
+  }
+}
+
+export default Header
---
+++ src/Search/index.js
@@ -0,0 +1,10 @@
+import React, { Component, Fragment } from 'react'
+import './search.css'
+
+class Search extends Component {
+  render() {
+    return <Fragment>Search</Fragment>
+  }
+}
+
+export default Search
